4f27f3da89afd581f617233e311eb651d4ce3938
Boilit <boilit@163.com>
2013-09-21 22:01:12 +0800

    add JvmMode

--- src/main/resources/benchmark.bat
+++ src/main/resources/benchmark.bat
@@ -1,9 +1,9 @@
 @rem Please give BenchmarkLocation.jar location to find engine's depend jars.
 @set Libraries=D:\W04WorkSpace\Maven001\ebm\lib\BenchmarkLocation.jar
 @rem Please give JAVA_HOME location to run benchmark
-@set JAVA_HOME=D:\Java\jdk7u25
+@set JAVA_HOME=D:\Java\jdk7u40x64
 @set PATH=.;%JAVA_HOME%\bin;%PATH%
 @set CLASSPATH=%JAVA_HOME%\lib\tool.jar;%JAVA_HOME%\lib\dt.jar;%JAVA_HOME%\jre\lib\rt.jar;%Libraries%;%CD%;%CLASSPATH%
-@set PARAMETERS= -warmCount 10 -loopCount 10000 -buffered false -outputEncoding UTF-8 -outputModel BYTES -capacity 2
-@java org.boilit.ebm.Main %PARAMETERS%
+@set PARAMETERS= -mode DEFAULT -warmCount 10 -loopCount 10000 -buffered false -outputEncoding UTF-8 -outputModel BYTES -capacity 2
+@%JAVA_HOME%\bin\java org.boilit.ebm.Main %PARAMETERS%
 @pause
