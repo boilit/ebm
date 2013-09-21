2879f3a4ea755d7a386c4be3e1be35c007506a74
于景洋 <boilit@163.com>
2013-09-21 13:09:27 +0800

    up benchmark.bat

--- src/main/resources/benchmark.bat
+++ src/main/resources/benchmark.bat
@@ -2,8 +2,8 @@
 @set Libraries=D:\W04WorkSpace\Maven001\ebm\lib\BenchmarkLocation.jar
 @rem Please give JAVA_HOME location to run benchmark
 @set JAVA_HOME=D:\Java\jdk7u25
-@set PATH=.;D:\Java\jdk7u25\bin;%PATH%
-@set CLASSPATH=D:\Java\jdk7u25\lib\tool.jar;D:\Java\jdk7u25\lib\dt.jar;D:\Java\jdk7u25\jre\lib\rt.jar;%Libraries%;%CD%;%CLASSPATH%
+@set PATH=.;%JAVA_HOME%\bin;%PATH%
+@set CLASSPATH=%JAVA_HOME%\lib\tool.jar;%JAVA_HOME%\lib\dt.jar;%JAVA_HOME%\jre\lib\rt.jar;%Libraries%;%CD%;%CLASSPATH%
 @set PARAMETERS= -warmCount 10 -loopCount 10000 -buffered false -outputEncoding UTF-8 -outputModel BYTES -capacity 2
 @java org.boilit.ebm.Main %PARAMETERS%
 @pause
